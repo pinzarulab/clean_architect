93c6003eb49c847e65dfaf06e47a6e8d8e8d7a4c
daniel.pinzaru <daniel.pinzaru@ebs-integrator.com>
2026-07-30 08:50:30 +0300

    Add initial dependency injection setup in di.dart and create .gitkeep files

---
+++ example/di/lib/di.dart
@@ -0,0 +1,10 @@
+import 'package:get_it/get_it.dart';
+
+
+Future<void> initDi({required GetIt get}) async {
+
+  // clean_architect:data-registrations
+
+
+  // clean_architect:domain-registrations
+}
